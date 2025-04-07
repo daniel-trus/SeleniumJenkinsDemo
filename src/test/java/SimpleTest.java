6c5b796681ba8fb3b11c3aa00d00676daf7b4128
Daniel Truszkowski <danieltruszkowski24@gmail.com>
2025-04-07 19:43:11 +0200

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Daniel\\IdeaProjects\\SeleniumJenkinsDemo\\src\\test\\resources\\ChromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Tytuł strony: " + driver.getTitle());
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
